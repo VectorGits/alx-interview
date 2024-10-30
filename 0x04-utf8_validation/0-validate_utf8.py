4e9c6b8d3681f2b23b44cb6faeed01b4b4c88ed7
VectorGits <davidoluremi0@gmail.com>
2024-10-30 19:59:26 +0100

    Refactor UTF-8 validation code for improved readability and fix indent bug

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,8 @@ method that determines if a given data set represents a valid UTF-8 encoding
 
 def validUTF8(data):
     """
-    method that determines if a given data set represents a valid UTF-8 encoding
+    method that determines if a given data set
+    represents a valid UTF-8 encoding
     """
     num_bytes = 0
 
@@ -17,17 +18,16 @@ def validUTF8(data):
                 if bit == '0':
                     break
                 num_bytes += 1
-                    
-                if num_bytes == 0:
-                    continue
 
-                if num_bytes == 1 or num_bytes > 4:
-                    return False
+            if num_bytes == 0:
+                continue
 
-            else:
-                if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
-                    return False
-            num_bytes -= 1
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
+                return False
+        num_bytes -= 1
 
     return num_bytes == 0
 
