c33b02069c5ca9f2175bfb73a76ab0ef4facbb0c
VectorGits <davidoluremi0@gmail.com>
2024-12-05 22:55:45 +0100

    Fix indentation

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,21 +3,21 @@
 
 
 def island_perimeter(grid):
-  """
-  Returns the perimeter of the island described in grid
-  """
+    """
+    Returns the perimeter of the island described in grid
+    """
 
-  perimeter = 0
+    perimeter = 0
 
-  for a in range(len(grid)):
-    for b in range(len(grid[a])):
-      if grid[a][b] == 1:
-        perimeter += 4
+    for a in range(len(grid)):
+        for b in range(len(grid[a])):
+            if grid[a][b] == 1:
+                perimeter += 4
 
-        if a > 0 and grid[a - 1][b] == 1:
-          perimeter -= 2
+            if a > 0 and grid[a - 1][b] == 1:
+                perimeter -= 2
 
-        if b > 0 and grid[a][b - 1] == 1:
-          perimeter -= 2
+            if b > 0 and grid[a][b - 1] == 1:
+                perimeter -= 2
 
-  return perimeter
+    return perimeter
