cef13c96dd9874ddce62dba4b3abc50df1191428
VectorGits <davidoluremi0@gmail.com>
2024-12-06 22:14:27 +0100

    Refactor island_perimeter function for improved readability

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python3
-"""Island Perimeter"""
-
-
 def island_perimeter(grid):
     """
     Returns the perimeter of the island described in grid
     """
-
     perimeter = 0
 
     for a in range(len(grid)):
@@ -14,10 +9,10 @@ def island_perimeter(grid):
             if grid[a][b] == 1:
                 perimeter += 4
 
-            if a > 0 and grid[a - 1][b] == 1:
-                perimeter -= 2
+                if a > 0 and grid[a - 1][b] == 1:
+                    perimeter -= 2
 
-            if b > 0 and grid[a][b - 1] == 1:
-                perimeter -= 2
+                if b > 0 and grid[a][b - 1] == 1:
+                    perimeter -= 2
 
     return perimeter
