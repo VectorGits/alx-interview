240c25bfbbe8394be48d57aafcdc78e87a1cb094
VectorGits <davidoluremi0@gmail.com>
2024-12-13 05:57:24 +0100

    Refactor prime game functions for improved readability and consistency

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-"""Prime Game"""
+""" Prime Game """
 
 
 def generatePrimeNumbers(limit):
-  """Generates a list of prime numbers stopping at limit"""
-  primeNumbers = []
-  sieveList = [True] * (limit + 1)
-  for potentialPrime in range(2, limit + 1):
-    if sieveList[potentialPrime]:
-      primeNumbers.append(potentialPrime)
-      for multiple in range(potentialPrime, limit + 1, potentialPrime):
-        sieveList[multiple] = False
-  return primeNumbers
+    """Generate list of primes up to limit"""
+    primeNumbers = []
+    sieveList = [True] * (limit + 1)
+    for potentialPrime in range(2, limit + 1):
+        if sieveList[potentialPrime]:
+            primeNumbers.append(potentialPrime)
+            for multiple in range(potentialPrime, limit + 1, potentialPrime):
+                sieveList[multiple] = False
+    return primeNumbers
 
 
 def isWinner(numRounds, roundValues):
-  """Determines the winner of the game"""
-  if not numRounds or not roundValues:
+    """
+    Determine the winner of the game
+    """
+    if not numRounds or not roundValues:
+        return None
+    mariaScore = benScore = 0
+    for i in range(numRounds):
+        primes = generatePrimeNumbers(roundValues[i])
+        if len(primes) % 2 == 0:
+            benScore += 1
+        else:
+            mariaScore += 1
+    if mariaScore > benScore:
+        return "Maria"
+    elif mariaScore < benScore:
+        return "Ben"
     return None
-  mariaScore = benScore = 0
-  for x in range(numRounds):
-    primes = generatePrimeNumbers(roundValues[x])
-    if len(primes) % 2 == 0:
-      benScore += 1
-    else:
-      mariaScore += 1
-  if mariaScore > benScore:
-    return "Maria"
-  elif mariaScore < benScore:
-    return "Ben"
-  return None
